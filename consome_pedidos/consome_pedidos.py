94e400de61e666e740b21fe09d5d8c956cbd1461
GabrielHCamarg0 <gabrielhcs1411@gmail.com>
2025-04-22 17:43:01 -0300

    Aulas de arquitetura de software sobre amqp

---
+++ consome_pedidos/consome_pedidos.py
@@ -0,0 +1,34 @@
+import pika, sys, os
+import json
+import json_tricks
+from pedido import Pedido
+
+
+def callback(ch, method, properties, body):
+    data = json_tricks.loads(body.decode("utf-8"))
+    print(data.pedido)
+
+def main():
+    credentials = pika.PlainCredentials('admin', 'admin')
+    parameters = pika.ConnectionParameters('localhost',
+                                       5672,
+                                       '/',
+                                       credentials)
+
+    connection = pika.BlockingConnection(parameters)
+
+    channel = connection.channel()
+    channel.queue_declare(queue='UFU')
+    channel.basic_consume(queue='UFU', on_message_callback=callback, auto_ack=True)
+    print(' [*] Aguardando mensagens. CTRL+C para sair')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
